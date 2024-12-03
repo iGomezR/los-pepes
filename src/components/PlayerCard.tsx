efaa738a25610c3fd03ce7fdb1d72165c0a3f860
Ivan Gomez <v.ivangomez@gmail.com>
2024-12-03 15:52:05 -0300

    github pages config

--- src/components/PlayerCard.tsx
+++ src/components/PlayerCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import { Card, CardContent, Typography, Box, Stack } from '@mui/material';
 import StarIcon from '@mui/icons-material/Star';
 import { Player } from '../interfaces/interfaces';
