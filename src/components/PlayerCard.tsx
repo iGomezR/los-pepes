96acf7e0bf6a4ea9a55cb78b62cb1451185c31a3
Ivan Gomez <v.ivangomez@gmail.com>
2024-12-03 16:07:10 -0300

    responsive card

--- src/components/PlayerCard.tsx
+++ src/components/PlayerCard.tsx
@@ -1,31 +1,43 @@
-// import React from 'react';
 import { Card, CardContent, Typography, Box, Stack } from '@mui/material';
 import StarIcon from '@mui/icons-material/Star';
 import { Player } from '../interfaces/interfaces';
 
-const PlayerCard = ({ player }: {player: Player}) => {
-  const { rank, name, stars, matchesPlayed, matchesWon, points } = player
+const PlayerCard = ({ player }: { player: Player }) => {
+  const { rank, name, stars, matchesPlayed, matchesWon, points } = player;
+
   return (
-    <Card sx={{ marginBottom: 1 }}>
-      <Stack direction="row" alignItems="center" spacing={1}>
-        {/* Imagen centrada a la izquierda */}
+    <Card sx={{ marginBottom: 2 }}>
+      <Stack
+        direction='row' // Column para pantallas pequeñas, row para más grandes
+        alignItems="center"
+      >
+        {/* Imagen centrada y responsiva */}
         <Box
           component="img"
           src={`${import.meta.env.BASE_URL}logo-pepes.png`}
           alt={`${name}'s avatar`}
           sx={{
-            width: 200,
-            height: 200,
-            borderRadius: '50%', // Para que sea circular
+            width: { xs: 100, sm: 150, md: 200 }, // Ajusta el tamaño según el tamaño de pantalla
+            height: { xs: 100, sm: 150, md: 200 },
+            borderRadius: '50%', // Circular
             objectFit: 'cover', // Ajustar la imagen
           }}
         />
         {/* Contenido de la tarjeta */}
-        <CardContent>
+        <CardContent
+          sx={{
+            textAlign: { xs: 'left', sm: 'left' }, // Centrado en pantallas pequeñas
+            flex: 1, // Asegura que ocupe el espacio necesario
+          }}
+        >
           <Typography variant="h6">
             {rank}. {name}
           </Typography>
-          <Stack direction="row" alignItems="center" spacing={1}>
+          <Stack
+            direction="row"
+            alignItems="center"
+            spacing={1}
+          >
             <Typography variant="body2">Estrellas:</Typography>
             <Box>
               {Array(stars)
